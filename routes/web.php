758204b9a5e33e5f7d2369140cddd036a069b0e1
OrlowskiDaniel <142877619+OrlowskiDaniel@users.noreply.github.com>
2026-03-12 16:29:32 +0100

    routes for all the admin and public pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home');
+
+Route::view('/menu', 'menu.index');
+
+Route::view('/contact', 'contact');
+
+Route::view('/reservations', 'orders.reservations.create');
+
+Route::view('/login', 'auth.login');
+
+Route::view('/cart', 'cart.index');
+
+
+
+Route::view('/admin', 'admin.dashboard');
+
+Route::view('/admin/dishes', 'admin.dishes.index');
+
+Route::view('/admin/dishes/create', 'admin.dishes.create');
+
+Route::view('/admin/reservations', 'admin.reservations.index');
+
+Route::view('/admin/orders', 'admin.orders.index');
+
+Route::view('/admin/messages', 'admin.messages.index');
